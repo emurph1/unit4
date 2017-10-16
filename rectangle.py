f842d1117c411651929859e02a654ec16cc062f0
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-16 14:32:18 -0400

    Updated from Brython Server: 10/16/2017 2:32:22 PM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,15 @@
+#Emily Murphy
+#2017-10-16
+#rectangle.py - plug in length and width and it prints out perimeter and area
 
+l = int(input('Enter length:'))
+w = int(input('Enter area:'))
+
+def area(l, w):
+    print(l*w)
+    
+def per(l,w):
+    print((l*2) + (w**2))
+    
+print('Area is', area(3,4))
+print('Perimeter is', per(3,4)
