d4b28f2b9e4d037390ca11bd1f44e25c5b29ca68
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-18 13:51:27 -0400

    Updated from Brython Server: 10/18/2017 1:51:28 PM

--- printSquares.py
+++ printSquares.py
@@ -6,6 +6,6 @@ def nums(num1,num2):
     for i in range(num1,num2):
         print('+--'*num2 + '+')
         print('|  '*num2 + '|')
-print('+--'*num2 + '+')
+    print('+--'*num2 + '+')
 
 nums(2,4)
