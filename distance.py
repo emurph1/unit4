e02448d154fb5a62ff0065b652b66116d8513149
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-18 13:57:17 -0400

    Updated from Brython Server: 10/18/2017 1:57:18 PM

--- distance.py
+++ distance.py
@@ -1,1 +1,10 @@
+#Emily Murphy
+#2017-10-18
+#distance.py - find distance between two points
 
+from math import sqrt
+
+def distance(x1,y1,x2,y2):
+    print(sqrt(((x2-x1)**2) +((y2-y1)**2)))
+
+distance(3,4,-5,2)
