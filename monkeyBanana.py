3e275c36dde2f88f7f3842192b8491893114614c
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-19 14:31:04 -0400

    Updated from Brython Server: 10/19/2017 2:31:12 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -5,12 +5,13 @@
 from ggame import *
 
 #constants
-ROWS = 30
+ROWS = 32
 COLS = 54
 CELL_SIZE = 20
 
 def moveRight(event):
-    monkey.x += CELL_SIZE
+    if monkey.x < (COLS-1)*CELL_SIZE:
+        monkey.x += CELL_SIZE
 
 def moveLeft(event):
     monkey.x -= CELL_SIZE
