a0884b3d49a34acc36bfdf16f560a030a931dc50
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-19 14:18:02 -0400

    Updated from Brython Server: 10/19/2017 2:18:11 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -13,9 +13,11 @@ if __name__ == '__main__':
     
     green = Color(0x006600,1)
     brown = Color(0x884513,1)
+    yellow = Color(0xFFFF00)
     
     jungleBox = RectangleAsset(COLS*CELL_SIZE,ROWS*CELL_SIZE,LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE, CELL_SIZE, LineStyle(1,brown),brown)
+    bananaBox = RectangleAsset(CELL_SIZE, CELL_SIZE, LineStyle(1,yellow),yellow)
     
     Sprite(jungleBox)
     Sprite(monkeyBox)
