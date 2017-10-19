1f49fb6717a680aa24e088db7155e5c7156d403b
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-19 14:47:22 -0400

    Updated from Brython Server: 10/19/2017 2:47:25 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     jungleBox = RectangleAsset(COLS*CELL_SIZE,ROWS*CELL_SIZE,LineStyle(1,green),green)
     monkeyBox = RectangleAsset(CELL_SIZE, CELL_SIZE, LineStyle(1,brown),brown)
     bananaBox = RectangleAsset(CELL_SIZE, CELL_SIZE, LineStyle(1,yellow),yellow)
-    scoreBox = TextAssest('Score = 0')
+    scoreBox = TextAsset('Score = 0')
     
     Sprite(jungleBox)
     banana = Sprite(bananaBox, ((COLS*CELL_SIZE)/2, (ROWS*CELL_SIZE)/2))
