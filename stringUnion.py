01eda1328d531b9ead00f2c514e3531b5455f458
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-20 14:36:54 -0400

    Updated from Brython Server: 10/20/2017 2:36:57 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,10 @@
+#Emily Murphy
+#2017-10-20
+#stringUnion.py -two strings and returns a string of each letter
 
+
+def stringUnion(word1, word2):
+    if ch in word1 == word2:
+        return str(ch)
+        
+stringUnion('Mississippi', 'Pennslyvania')
