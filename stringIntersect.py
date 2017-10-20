6e18f5c9ac98e1c7475f969867c7be87fef8d10c
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-20 14:52:57 -0400

    Update stringIntersect.py

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,16 @@
+#Emily Murphy
+#2017-10-20
+#stringIntersect.py -two strings and returns a string of each letter
 
+
+def stringUnion(word1, word2):
+    answer = ' '
+    for ch in word1:
+        if ch not in answer:
+            answer += ch
+    for ch in word2:
+        if ch not in answer:
+            answer += ch
+    return answer
+        
+print(stringUnion('Mississippi', 'Pennslyvania'))
