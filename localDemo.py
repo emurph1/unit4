5b60509cfd566317e958e883a0cc57e0dc336f02
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-23 14:17:47 -0400

    Updated from Brython Server: 10/23/2017 2:17:49 PM

--- localDemo.py
+++ localDemo.py
@@ -3,8 +3,9 @@
 #localDemo.py - learn how to us local variables
 
 def f():
-    x = 77
+    x = 77 #x is a local variable 
+    y = 10 #y is a local variable
 
 x = 5
-f()
-
+f() #x does not change
+print(x)
