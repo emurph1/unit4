b16002e7899a07f5aa49605ec832df8c79c5a4a0
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-23 14:18:58 -0400

    Updated from Brython Server: 10/23/2017 2:19:00 PM

--- localDemo.py
+++ localDemo.py
@@ -2,6 +2,8 @@
 #2017-10-23
 #localDemo.py - learn how to us local variables
 
+#local variables are ones that are declared in the function
+
 def f():
     x = 77 #x is a local variable 
     y = 10 #y is a local variable
