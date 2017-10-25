f3f402facda61b6963a5dd2a387b84acd13458b4
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-25 13:59:11 -0400

    Updated from Brython Server: 10/25/2017 1:59:14 PM

--- triangle.py
+++ triangle.py
@@ -8,13 +8,16 @@ x1 = int(input('x1:'))
 x2 = int(input('x2:'))
 y1 = int(input('y1:'))
 y2 = int(input('y2:'))
+x3 = int(input('x3:'))
+y3 = int(input('y3:'))
 
 def distance(x1,y1,x2,y2):
     return (sqrt(((x2-x1)**2) +((y2-y1)**2)))
     
 
-
-area(x1,y1,x2,y2)
+a = distance(x1,y1,x2,y2)
+b = distance(x1, y1,x3,y3)
+c = distance(x2,y2,x3,y3)
 
 
 
