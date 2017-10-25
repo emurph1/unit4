2d976a32d74c9757def72698ceec5f2fa4991f5c
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-25 16:56:15 -0400

    Updated from Brython Server: 10/25/2017 4:56:19 PM

--- triangle.py
+++ triangle.py
@@ -5,10 +5,10 @@
 from math import sqrt
 
 x1 = int(input('x1:'))
-x2 = int(input('x2:'))
-x3 = int(input('x3:'))
 y1 = int(input('y1:'))
+x2 = int(input('x2:'))
 y2 = int(input('y2:'))
+x3 = int(input('x3:'))
 y3 = int(input('y3:'))
 
 def distance(x1,y1,x2,y2):
