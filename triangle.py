f4987330be166ab33bc12b92351bbc4483737074
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-25 13:52:42 -0400

    Updated from Brython Server: 10/25/2017 1:52:46 PM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,23 @@
+#Emily Murphy
+#2017-10-25
+#triangle.py - input vertices finds area
+
+from math import sqrt
+
+x1 = int(input('x1:'))
+x2 = int(input('x2:'))
+y1 = int(input('y1:'))
+y2 = int(input('y2:'))
+
+def distance(x1,y1,x2,y2):
+    return (sqrt(((x2-x1)**2) +((y2-y1)**2)))
+    
+
+
+area(x1,y1,x2,y2)
+
+
+
+
+    
 
