81b85d8a7a54b4877af2399e3fc429116ada07a8
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:07:48 -0400

    Updated from Brython Server: 10/26/2017 2:07:54 PM

--- recursionDemo.py
+++ recursionDemo.py
@@ -1,6 +1,8 @@
 #Emily Murphy
 #2017-10-26
-#recursionDemo.py - recursions
+#recursionDemo.py - recursive version of countdown function
 
-
-
+def countdown(n):
+    print('Boom!')
+    
+countdown(5)
