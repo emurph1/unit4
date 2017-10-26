b5f4e18ae0106013d8553f7e232c76d698b680ec
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:24:44 -0400

    Updated from Brython Server: 10/26/2017 2:24:50 PM

--- multiply.py
+++ multiply.py
@@ -5,18 +5,18 @@
 from random import randint
 
 numCorrect = 0
+def encourage():
+    if numcorrect == 5:
+        numcorrect == 0
+        print('You are doing great! Keep going')
 while True:
     num1 = randint(1,12)
     num2 = randint(1,12)
     question = 'What is ' + str(num1) + ' * ' + str(num2) + '?'
     answer = int(input(question))
     if num1 * num2 == answer:
-        print('Correct')
+        print(encourage)
         numCorrect += 1
     else: 
         print('Incorrect. Try again')
 
-def encourage():
-    if numcorrect == 5:
-        numcorrect == 0
-        print('You are doing great! Keep going')
