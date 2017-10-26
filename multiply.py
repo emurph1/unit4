8d03782fa99027c99c0d0a4ba3bb60514875fb92
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:24:03 -0400

    Updated from Brython Server: 10/26/2017 2:24:10 PM

--- multiply.py
+++ multiply.py
@@ -8,11 +8,13 @@ numCorrect = 0
 while True:
     num1 = randint(1,12)
     num2 = randint(1,12)
-    question = 'What is ' + str(num1) + ' + ' + str(num2) + '?'
+    question = 'What is ' + str(num1) + ' * ' + str(num2) + '?'
     answer = int(input(question))
-    if num1 + num2 == answer:
+    if num1 * num2 == answer:
         print('Correct')
         numCorrect += 1
+    else: 
+        print('Incorrect. Try again')
 
 def encourage():
     if numcorrect == 5:
