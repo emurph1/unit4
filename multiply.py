4c8e4d1927b45c5ec7751c52849d89ec850a05fc
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:36:13 -0400

    Updated from Brython Server: 10/26/2017 2:36:19 PM

--- multiply.py
+++ multiply.py
@@ -21,7 +21,7 @@ while True:
     num1 = randint(1,12)
     num2 = randint(1,12)
     question = 'What is ' + str(num1) + ' * ' + str(num2) + '?'
-    answer = int(input(question))
+    answer = int(input(question + ' '))
     if num1 * num2 == answer:
         numCorrect += 1
         if numCorrect == 5:
