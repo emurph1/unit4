b2013e147162c7853421a40c07cf229c85c308e9
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:47:49 -0400

    Updated from Brython Server: 10/26/2017 2:47:55 PM

--- bubbles.py
+++ bubbles.py
@@ -1,1 +1,25 @@
+#Emily Murphy
+#2017-10-26
+#bubbles.py - opens window and click causes bubbles to appear
 
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+white = Color(0xFFFFFF,1)
+
+def mouseClick(Event):
+    num = randint(1,4)
+    if num == 1:
+        Sprite(blueRectangle)
+    elif num == 2:
+        Sprite(greenRectangle)
+    elif num == 3:
+        Sprite(blackRectangle)
+    elif num == 4:
+        Sprite(redRectangle)
+        
+
+App().listenMouseEvent('click',mouseClick)
+App().run()
