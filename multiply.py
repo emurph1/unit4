7262dee9444549243fa7dc98ea8079b813389264
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:20:24 -0400

    Updated from Brython Server: 10/26/2017 2:20:30 PM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,17 @@
+#Emily Murphy
+#2017-10-26
+#multiply.py - teach kids how to multiply
+
+from random import randint
+
+numCorrect = 0
+while numCorrect < 5:
+    num1 = randint(1,12)
+    num2 = randint(1,12)
+    question = 'What is ' + str(num1) + ' + ' + str(num2) + '?'
+    answer = int(input(question))
+    if num1 + num2 == answer:
+        print('Correct')
+        numCorrect += 1
+
 
