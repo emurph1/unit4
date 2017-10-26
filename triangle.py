92f9c9bea2b05fcb85c418f9c056b3f8a2faed34
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-26 14:14:56 -0400

    Updated from Brython Server: 10/26/2017 2:15:02 PM

--- triangle.py
+++ triangle.py
@@ -17,8 +17,9 @@ def distance(x1,y1,x2,y2):
 a = distance(x1,y1,x2,y2)
 b = distance(x1, y1,x3,y3)
 c = distance(x2,y2,x3,y3)
-area = (sqrt(s*(s-a)*(s-b)*(s-c)))
 s = .5*(a + b + c)
+area = (sqrt(s*(s-a)*(s-b)*(s-c)))
+
 
 print(area)
 
