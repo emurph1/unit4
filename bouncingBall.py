48487c8fd5b297fae289c479a04627b79af37f3e
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-27 14:54:43 -0400

    Updated from Brython Server: 10/27/2017 2:54:51 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -3,6 +3,7 @@
 #bouncingBall.py - ball bounces in frame
 
 from ggame import *
+from random import randint
 
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
@@ -12,11 +13,21 @@ whiteOutline = LineStyle(1, white)
 blackOutline = LineStyle(10, black)
 
 blackRectangle = RectangleAsset(900, 600, blackOutline, black)
-blueCircle = CircleAsset(100, blackOutline, blue)
 
 
+circle = blueCircle = CircleAsset(100, blackOutline, blue)
+
+def moveCircle():
+    banana.x = randint(0, COLS-1)*CELL_SIZE
+    banana.y = randint(0, ROWS-1)*CELL_SIZE
+    data['frames'] = 0
+
+def step():
+    data['frames'] += 1
+    if data['frames'] == 200:
+        moveBanana()
 
 Sprite(blackRectangle)
 Sprite(blueCircle,(200,300))
 
-App().run()
+App().run(step)
