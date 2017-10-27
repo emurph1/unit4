08892970b5be5275f45b5688c278fe82c54d808e
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-27 14:48:37 -0400

    Updated from Brython Server: 10/27/2017 2:48:45 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -8,7 +8,7 @@ blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 white = Color(0xFFFFFF,1)
 
-whiteOutine = LineStyle(1, white)
+whiteOutline = LineStyle(1, white)
 blackOutline = LineStyle(10, black)
 
 blueRectangle = RectangleAsset(900, 500, whiteOutline, blue)
