bf08576026afcfe6fb6f30067bda18bc721ff319
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-27 14:47:14 -0400

    Updated from Brython Server: 10/27/2017 2:47:22 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -1,1 +1,16 @@
+#Emily Murphy
+#2017-10-27
+#bouncingBall.py - ball bounces in frame
 
+from ggame import *
+
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+
+
+blackOutline = LineStyle(10, black)
+
+blueRectangle = RectangleAsset(500, 700, blackOutine, blue)
+
+Sprite(blueRectangle)
+App().run()
