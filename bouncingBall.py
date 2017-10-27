a8a7c7e69e53b24ba77dad7fb3ba886d31d79687
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-27 14:47:49 -0400

    Updated from Brython Server: 10/27/2017 2:47:57 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -7,10 +7,11 @@ from ggame import *
 blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 
-
 blackOutline = LineStyle(10, black)
 
 blueRectangle = RectangleAsset(500, 700, blackOutine, blue)
 
 Sprite(blueRectangle)
+
+
 App().run()
