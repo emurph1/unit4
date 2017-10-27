2da399a8aa9bb2659813299563cdbfe2783a49e5
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-27 14:44:42 -0400

    Updated from Brython Server: 10/27/2017 2:44:50 PM

--- gcf.py
+++ gcf.py
@@ -5,8 +5,8 @@
 def gcf(num1,num2):
     x = min(num1,num2)
     for i in range(x,0,-1):
-        if num1%x == 0 and num2%x == 0:
-            return x
+        if num1%i == 0 and num2%i == 0:
+            return i
             
 print(gcf(10,15))
         
