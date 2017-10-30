ca36fbc70cc530b7eefda752ffdb9fb57aae8e1d
emurph1 <31485900+emurph1@users.noreply.github.com>
2017-10-30 14:36:07 -0400

    Updated from Brython Server: 10/30/2017 2:36:10 PM

--- quiz4.py
+++ quiz4.py
@@ -14,9 +14,9 @@ def average(num1, num2, num3):
 
 print(average(1,2,3))
 
-def lastLetter(word):
-    for ch in word:
-        print(ch)   
+def lastLetter(letter):
+    for ch in letter:
+        letter += ch
 lastLetter('good')
 
 def same(x,y):
